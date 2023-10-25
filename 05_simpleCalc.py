4599745d4409a38118f4dd82893bbf6a0eef2047
Parth Sidpara <parthsidpara2005@gmail.com>
2023-10-25 19:05:30 +0530

    Format file Five

--- 05_simpleCalc.py
+++ 05_simpleCalc.py
@@ -6,21 +6,25 @@ root.title("Simple Calculator")
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
+
 def button_click(number):
     now = e.get()
     e.delete(0, END)
     e.insert(0, str(now) + str(number))
 
+
 def button_clear():
     e.delete(0, END)
-    
+
+
 def button_add():
     first_num = e.get()
     global f_num
-    global math 
+    global math
     math = "addition"
     f_num = int(first_num)
     e.delete(0, END)
+
 
 def button_equal():
     second_number = e.get()
@@ -38,31 +42,32 @@ def button_equal():
     if math == "division":
         e.insert(0, f_num / int(second_number))
 
-    
 
 def button_subtract():
     first_num = e.get()
     global f_num
-    global math 
+    global math
     math = "subtraction"
     f_num = int(first_num)
     e.delete(0, END)
 
+
 def button_multiply():
     first_num = e.get()
     global f_num
-    global math 
+    global math
     math = "multiplication"
     f_num = int(first_num)
     e.delete(0, END)
 
+
 def button_divide():
     first_num = e.get()
     global f_num
-    global math 
+    global math
     math = "division"
     f_num = int(first_num)
-    e.delete(0, END) 
+    e.delete(0, END)
 
 
 # Buttons adding
@@ -78,7 +83,7 @@ button_8 = Button(text="8", padx=30, pady=30, command=lambda: button_click(8))
 button_9 = Button(text="9", padx=30, pady=30, command=lambda: button_click(9))
 button_0 = Button(text="0", padx=30, pady=30, command=lambda: button_click(0))
 
-button_equal = Button(text="=", padx=70 , pady=30, command=button_equal)
+button_equal = Button(text="=", padx=70, pady=30, command=button_equal)
 button_clear = Button(text="Clear", padx=60, pady=30, command=button_clear)
 button_add = Button(text="+", padx=30, pady=30, command=button_add)
 
@@ -89,23 +94,23 @@ button_divide = Button(text="/", padx=31, pady=30, command=button_divide)
 
 # Buttons placing
 
-button_1.grid(row=1,column=0)
-button_2.grid(row=1,column=1)
-button_3.grid(row=1,column=2)
+button_1.grid(row=1, column=0)
+button_2.grid(row=1, column=1)
+button_3.grid(row=1, column=2)
 
-button_5.grid(row=2,column=1)
-button_6.grid(row=2,column=2)
-button_4.grid(row=2,column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+button_4.grid(row=2, column=0)
 
-button_7.grid(row=3,column=0)
-button_8.grid(row=3,column=1)
-button_9.grid(row=3,column=2)
+button_7.grid(row=3, column=0)
+button_8.grid(row=3, column=1)
+button_9.grid(row=3, column=2)
 
-button_0.grid(row=4,column=0)
+button_0.grid(row=4, column=0)
 button_equal.grid(row=5, column=1, columnspan=2)
 button_add.grid(row=5, column=0)
 button_clear.grid(row=4, column=1, columnspan=2)
 button_subtract.grid(row=6, column=0)
 button_multiply.grid(row=6, column=1)
 button_divide.grid(row=6, column=2)
-root.mainloop() 
+root.mainloop()
